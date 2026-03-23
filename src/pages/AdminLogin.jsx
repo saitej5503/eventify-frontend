1908392486b35a39d19f45ec485dbb2760085d28
Saiteja <pulisettysaiteja@gmail.com>
2026-03-24 02:59:42 +0530

    fixed api url

--- src/pages/AdminLogin.jsx
+++ src/pages/AdminLogin.jsx
@@ -12,7 +12,7 @@ export default function AdminLogin() {
     e.preventDefault();
 
     try {
-      const res = await fetch("http://localhost:5000/api/admin/login", {
+      const res = await fetch("https://eventify-backend-rxdz.onrender.com/api/admin/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
